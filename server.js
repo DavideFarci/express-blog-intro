7a61ca9aedb4d4a6d450a96c636b44f41d0a83f1
DavideFarci <yank92.df@gmail.com>
2023-11-10 14:20:57 +0100

    create server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+/**
+ * @param {express.Request} Req
+ * @param {express.Response} Res
+ */
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+// Rotte
+app.get("/", homeController.index);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running in port http://localhost:" + process.env.PORT);
+});
